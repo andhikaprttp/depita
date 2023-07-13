2fce9d31e25038cdf0710a610cf60ce7052a1f2e
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-07-13 16:07:17 +0700

    update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const hitung = () => {
   const angka1 = document.getElementById("angka1").value;
   const angka2 = document.getElementById("angka2").value;
   const sistem = document.getElementById("sistem").value;
-  
+
   let hasil = "";
 
-  // Fungsi membuat sistem kalkulator 
+  // Fungsi membuat sistem kalkulator
   if (sistem === "biner") {
     hasil = parseInt(angka1, 2) + parseInt(angka2, 2);
   } else if (sistem === "desimal") {
@@ -14,7 +14,7 @@ const hitung = () => {
   } else if (sistem === "hexadesimal") {
     hasil = (parseInt(angka1, 16) + parseInt(angka2, 16)).toString(16);
   }
-  
+
   // Fungsi menampilkan hasil
   document.getElementById("hasil").value = hasil;
 };
