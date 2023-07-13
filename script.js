c0f42fc7d577e4c356c16bf491832ba42ddb71b9
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-07-13 14:15:04 +0700

    update script.js

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const hitung = () => {
   const angka2 = document.getElementById("angka2").value;
   const sistem = document.getElementById("sistem").value;
   let hasil = "";
-  
-    if (sistem === 'biner') {
-      hasil = parseInt(angka1, 2) + parseInt(angka2, 2);
-    } else if (sistem === 'desimal') {
-      hasil = parseInt(angka1, 10) + parseInt(angka2, 10);
-    } else if (sistem === 'hexadesimal') {
-      hasil = (parseInt(angka1, 16) + parseInt(angka2, 16)).toString(16);
-    }
-    
-    document.getElementById('hasil').value = hasil; 
-    
+
+  if (sistem === "biner") {
+    hasil = parseInt(angka1, 2) + parseInt(angka2, 2);
+  } else if (sistem === "desimal") {
+    hasil = parseInt(angka1, 10) + parseInt(angka2, 10);
+  } else if (sistem === "hexadesimal") {
+    hasil = (parseInt(angka1, 16) + parseInt(angka2, 16)).toString(16);
+  }
+
+  document.getElementById("hasil").value = hasil;
 };
