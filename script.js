024edc62c6f6aa9ed0875ac0130437a36bf5ba14
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-07-13 14:00:22 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hitung = () => {
-  const angka1 = document.getElementById('angka1').value;
-  const angka2 = document.getElementById('angka2').value;
-  const sistem = document.getElementById('sistem').value; 
-  let result = '';
-}
+  const angka1 = document.getElementById("angka1").value;
+  const angka2 = document.getElementById("angka2").value;
+  const sistem = document.getElementById("sistem").value;
+  let result = "";
+};
