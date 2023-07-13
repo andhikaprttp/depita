d9f5e2a834c1447e833e67db71430be08d26f9e8
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-07-13 14:26:01 +0700

    update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const hitung = () => {
   } else if (sistem === "hexadesimal") {
     hasil = (parseInt(angka1, 16) + parseInt(angka2, 16)).toString(16);
   }
-
+  
+  // Fungsi menampilkan hasil
   document.getElementById("hasil").value = hasil;
 };
